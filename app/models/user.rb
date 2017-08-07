de3f81faa64f5fb4c495db197087a0d21f3fdb9c
Carolina Meira <meira.carolina@gmail.com>
2017-08-07 17:49:07 -0300

    De volta para a convenção rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :groups
   has_many :meetings, through: :groups
+  has_many :coach_meetings, class_name: "Meeting"
   has_many :reviews
 end
